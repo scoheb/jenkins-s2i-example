29620b0a911bf69b11c8366773d36c4c6991c114
Scott Hebert <shebert@redhat.com>
2017-06-13 15:02:09 -0400

    add missing import

--- master/configuration/init.groovy
+++ master/configuration/init.groovy
@@ -1,9 +1,9 @@
 import java.util.logging.Logger
 import jenkins.security.s2m.*
+import jenkins.model.*;
 def logger = Logger.getLogger("")
 logger.info("Disabling CLI over remoting")
 jenkins.CLI.get().setEnabled(false);
 logger.info("Enable Slave -> Master Access Control")
-Jenkins.instance.injector.getInstance(AdminWhitelistRule.class)
-    .setMasterKillSwitch(false);
+Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
 Jenkins.instance.save()
